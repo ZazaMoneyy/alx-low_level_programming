cf0dbca7f09a53cd53efb57d42e5c5c5b4f7ff7b
sheriff lukman <sheriff2741@gmail.com>
2023-09-12 11:10:35 -0700

    Functions and Nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - sign number
+ * @n: The number to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
@@ -12,12 +13,12 @@ if (n > 0)
 {
 	_putchar('+');
 	return (1);
-} 
+}
 else if (n == 0)
 {
 	_putchar(48);
 	return (0);
-} 
+}
 else if (n < 0)
 {
 	_putchar('-');
