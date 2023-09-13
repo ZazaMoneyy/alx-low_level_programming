1b324c2504615e71ff58cb053b22743f6f87eaec
sheriff lukman <sheriff2741@gmail.com>
2023-09-12 17:50:43 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,45 +4,28 @@
  * main - Entry point for the Fibonacci program.
  *
  * Description:
- * This program calculates and prints the Fibonacci sequence up to a certain
- * point while avoiding hardcoding. It also handles large numbers by splitting
- * them into two parts, 'new1' and 'new2' for numbers greater than or equal to
- * 1 billion, and similarly for 'before1' and 'before2'.
+ * This program calculates and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, separated by commas.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int new1 = 1;
-	unsigned long int new2 = 2;
-	unsigned long int before1 = 0;
-	unsigned long int before2 = 1;
-	unsigned long int limit = 1000000000;
-	unsigned long int temp1, temp2;
+	unsigned long int current = 1;
+	unsigned long int previous = 1;
+	unsigned long int temp;
+	int count;
 
-	printf("%lu, %lu", new1, new2);
+	printf("%lu", current);
 
-	for (i = 2; i < 98; i++)
+	for (count = 2; count <= 98; count++)
 {
-		printf(", %lu", new1 + new2);
+		printf(", %lu", current);
 
 	/* Calculate the next Fibonacci number */
-		temp1 = new1;
-		temp2 = new2;
-
-		new1 = new1 + before1;
-		before1 = temp1;
-
-	/* Handle large numbers by splitting into two parts */
-		new2 = new2 + before2;
-		before2 = temp2;
-
-		if (new2 >= limit)
-{
-			new2 -= limit;
-			new1 += 1;
-}
+		temp = current;
+		current += previous;
+		previous = temp;
 }
 
 	printf("\n");
