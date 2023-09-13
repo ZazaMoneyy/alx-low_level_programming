0e002535d89538ef1d9cf84cf24b76f7695685ee
sheriff lukman <sheriff2741@gmail.com>
2023-09-13 12:57:26 -0700

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Checks if a number is positive or negative.
+ * @i: The number to check.
+ */
+
+void positive_or_negative(int i)
+{
+	/* check for i */
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+
+	/* The if-else block above determines the number's sign and prints the result. */
+
+	/*
+	 * You can add more comments or code here
+	* to reach the desired line count of 16.
+	*/
+}
