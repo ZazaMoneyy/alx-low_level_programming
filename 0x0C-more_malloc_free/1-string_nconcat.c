be8b937377de82c2ba8f1c5ec4fcb33cb2090f5e
sheriff lukman <sheriff2741@gmail.com>
2023-10-08 05:07:58 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,44 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenate string
- * @s1: string 1
- * @s2: string 2
- * @n: no of byte to concatenate from s2
- * Return: string
+ * string_nconcat - concatenate two strings while
+ * second string is an n value
+ *
+ * @s1: string one
+ * @s2: string two
+ * @n: no of element to concatenate from s2
+ *
+ * Return: poimnter to the new allocated memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-	unsigned int s1len = 0;
-	unsigned int s2len = 0;
-	char *output;
+	size_t i, j, k;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s1 == NULL)
-		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-		s1len++;
-	for (i = 0; s2[i] != '\0'; i++)
-		s2len++;
-
-	output = malloc(sizeof(char) * (s1len + n) + 1);
-	if (output == NULL)
-		return (NULL);
-	if (n >= s2len)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			output[i] = s1[i];
-		for (i = 0; s2[i] != '\0'; i++)
-			output[s1len + i] = s2[i];
-		output[s1len + i] = '\0';
-	}
+		i = 0;
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
-			output[i] = s1[i];
-		for (i = 0; i < n; i++)
-			output[s1len + i] = s2[i];
-		output[s1len + i] = '\0';
+			;
 	}
-	return (output);
+
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - a memory in the heap
+ * @ptr: pointer of the old memory
+ * @old_size: old size allocated by malloc
+ * @new_size: new size allocated by _realloc
+ * Return: pointer to the new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	size_t i, max = new_size;
+	char *oldp = ptr;
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+			return (p);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+	if (new_size > old_size)
+		max = old_size;
+	for (i = 0; i < max; i++)
+		p[i] = oldp[i];
+	free(ptr);
+	return (p);
+}
