47855d14d8cf300acb0413c4d097ccd606016410
sheriff lukman <sheriff2741@gmail.com>
2023-10-19 04:39:28 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - elements in a linked list
+ * @h: pointer to the list_t len
+ *
+ * Return: print elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list
+ * @str: string in the node
+ *
+ * Return: print address of the new element, or NULL if fail
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	newnode = malloc(sizeof(list_t));
+	if (!newnode)
+		return (NULL);
+
+	newnode->str = strdup(str);
+	newnode->len = len;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = newnode;
+
+	return (newnode);
+}
